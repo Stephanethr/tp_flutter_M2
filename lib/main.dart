6e07be355a6a0b3f5e67186ee788f2708ed18ec2
stephanethr <stethiry9@gmail.com>
2025-10-20 20:31:06 +0200

    j'ai viré les clés api et mis dans un .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // <-- ajout pour .env
 import 'firebase_options.dart';
 import 'pages/splash_screen.dart';
 import 'constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Charger les variables d'environnement depuis le fichier .env
+  await dotenv.load(fileName: ".env");
+
+  // Initialiser Firebase avec les options selon la plateforme
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ChatApp());
 }
 
